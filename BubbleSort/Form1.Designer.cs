72d79b3265a48918a03ba5c72dfa8e831b02b7b2
samiul1111 <samiul1111@gmail.com>
2020-09-28 01:02:05 +0600

    first commit

---
+++ BubbleSort/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace BubbleSort
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(77, 276);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "5";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(252, 276);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 45);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "10";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(417, 276);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(145, 45);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "15";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(577, 276);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(144, 45);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "20";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(740, 276);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(144, 45);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(901, 276);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(144, 45);
+            this.button6.TabIndex = 3;
+            this.button6.Text = "30";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(851, 577);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(239, 45);
+            this.btnNext.TabIndex = 6;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(947, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1193, 692);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ BubbleSort/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BubbleSort
+{
+    public enum StepsEnum
+    {
+        Start = 0,
+        Selection = 1,
+        Comparison = 2,
+        Success = 3,
+        Failure = 4
+    }
+    public partial class Form1 : Form
+    {
+        List<Button> allBtns =new List<Button>();
+        private StepsEnum currentStep;
+        private Button firstButton, secondButton;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            allBtns.Add(button1);
+            allBtns.Add(button2);
+            allBtns.Add(button3);
+            allBtns.Add(button4);
+            allBtns.Add(button5);
+            allBtns.Add(button6);
+
+            currentStep = StepsEnum.Start;
+        }
+
+        private int i = -1, j = -1, totalIteration = 1, prevX, count = 0;
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            for (int k = 0; k < allBtns.Count; k++)
+            {
+                allBtns[k].BackColor = Color.DarkCyan;
+            }
+
+            if (currentStep == StepsEnum.Start)
+            {
+                Start();
+            }
+            else if (currentStep == StepsEnum.Selection)
+            {
+                Selection();
+            }
+
+            else if (currentStep == StepsEnum.Comparison)
+            {
+                CompareValue();
+            }
+            else if (currentStep == StepsEnum.Failure)
+            {
+                currentStep = StepsEnum.Selection;
+                Selection();
+            }
+            else if (currentStep == StepsEnum.Success)
+            {
+                var temp = allBtns[i];
+                allBtns[i] = allBtns[j];
+                allBtns[j] = temp;
+
+                prevX = firstButton.Location.X;
+                count= 1;
+
+                btnNext.Enabled = false;
+                timer1.Start();
+
+            }
+            label1.Text = totalIteration.ToString();
+        }
+
+        private void CompareValue()
+        {
+            int f, s;
+            f = Int32.Parse(firstButton.Text);
+            s = Int32.Parse(secondButton.Text);
+
+            if (f > s)
+            {
+                firstButton.BackColor = secondButton.BackColor = Color.DarkGreen;
+                currentStep = StepsEnum.Success;
+            }
+            else
+            {
+                firstButton.BackColor = secondButton.BackColor = Color.DarkRed;
+                currentStep = StepsEnum.Failure;
+            }
+        }
+
+        private void Selection()
+        {
+            if (i == -1 && j == -1)
+            {
+                i = 0;
+                j = 1;
+            }
+
+            else if (totalIteration > (allBtns.Count * allBtns.Count)-1)
+            {
+                btnNext.Enabled = false;
+            }
+
+            else if (totalIteration % (allBtns.Count - 1) == 0)
+            {
+                i = 0;
+                j = 1;
+                totalIteration++;
+            }
+
+            else
+            {
+                i++;
+                j++;
+                totalIteration++;
+            }
+
+            firstButton = allBtns[i];
+            secondButton = allBtns[j];
+
+            firstButton.BackColor = secondButton.BackColor = Color.DarkBlue;
+
+            currentStep = StepsEnum.Comparison;
+        }
+
+        private void Start()
+        {
+            currentStep = StepsEnum.Selection;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (firstButton.Location.X == prevX && count <= 5)
+            {
+                firstButton.Location = new Point(firstButton.Location.X, firstButton.Location.Y - 5);
+                secondButton.Location = new Point(secondButton.Location.X, secondButton.Location.Y + 5);
+                count++;
+            }
+            else if (secondButton.Location.X > prevX)
+            {
+                firstButton.Location = new Point(firstButton.Location.X + 5, firstButton.Location.Y);
+                secondButton.Location = new Point(secondButton.Location.X - 5, secondButton.Location.Y);
+            }
+            else if (firstButton.Location.X != prevX && count > 1)
+            {
+                firstButton.Location = new Point(firstButton.Location.X, firstButton.Location.Y + 5);
+                secondButton.Location = new Point(secondButton.Location.X, secondButton.Location.Y - 5);
+                count--;
+            }
+            else
+            {
+                timer1.Stop();
+                btnNext.Enabled = true;
+                currentStep = StepsEnum.Selection;
+            }
+        }
+
+        
+    }
+}
